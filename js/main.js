c2516c55695096b6d4b27af1803acd2bd2b4b514
sm3shnoy <eskey96@inbox.ru>
2019-09-24 10:51:48 +0400

    Правка ошибок с тревисом

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+'use strict';
+
 // Переменные для объектов в предложении (offer)
-var TITLE = "Заголовок";
+var TITLE = 'Заголовок';
 var PRICE = 100000;
-var TYPE = "palace";
+var TYPE = 'palace';
 var ROOMS = 3;
 var GUESTS = 5;
-var CHECKIN = "12:00";
-var CHECKOUT = "13:00";
-var FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var DESCRIPTION = "Описание";
-var PHOTO = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+var CHECKIN = '12:00';
+var CHECKOUT = '13:00';
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var DESCRIPTION = 'Описание';
+var PHOTO = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 // Координаты для позиционирования (location)
 var minY = 130;
@@ -29,7 +31,7 @@ var similarPinTemplate = document.querySelector('#pin')
 var randomNumber = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
-}
+};
 
 // Генерируем объекты объявлений
 var adsGenerator = function () {
@@ -37,48 +39,47 @@ var adsGenerator = function () {
 
   for (var i = 0; i <= COUNTER_ADS; i++) {
     ads[i] = {
-      "author": {
-        "avatar": "img/avatars/user" + "0" + i + ".png"
+      'author': {
+        'avatar': 'img/avatars/user' + '0' + i + '.png'
       },
 
-      "offer": {
-        "title": TITLE,
-        "address": "{{location.x}}, {{location.y}}",
-        "price": PRICE,
-        "type": TYPE[0],
-        "rooms": ROOMS,
-        "guests": GUESTS,
-        "checkin": CHECKIN,
-        "checkout": CHECKOUT,
-        "features": FEATURES[0],
-        "description": DESCRIPTION,
-        "photos": PHOTO
+      'offer': {
+        'title': TITLE,
+        'address': '{{location.x}}, {{location.y}}',
+        'price': PRICE,
+        'type': TYPE[0],
+        'rooms': ROOMS,
+        'guests': GUESTS,
+        'checkin': CHECKIN,
+        'checkout': CHECKOUT,
+        'features': FEATURES[0],
+        'description': DESCRIPTION,
+        'photos': PHOTO
       },
 
-      "location": {
-        "x": randomNumber(minX, maxX),
-        "y": randomNumber(minY, maxY)
+      'location': {
+        'x': randomNumber(minX, maxX),
+        'y': randomNumber(minY, maxY)
       }
-    }
+    };
   }
 
   return ads;
 };
 
 // Убираем класс faded у блока map
-var map = document.querySelector(".map");
-map.classList.remove("map--faded");
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
 // Все объявления
 var ads = adsGenerator();
 
 var renderedAds = function (ad) {
   var adsElement = similarPinTemplate.cloneNode(true);
-  var log = "left: " + ad.location.x + "px;" + "top: " + ad.location.y + "px;"
 
-  adsElement.querySelector('.map__pin').style = "left: " + ad.location.x + "px;" + "top: " + ad.location.y + "px;";
-  adsElement.querySelector('.map__pin img').src= ad.author.avatar;
-  adsElement.querySelector('.map__pin img').alt= ad.offer.title;
+  adsElement.querySelector('.map__pin').style = 'left: ' + ad.location.x + 'px;' + 'top: ' + ad.location.y + 'px;';
+  adsElement.querySelector('.map__pin img').src = ad.author.avatar;
+  adsElement.querySelector('.map__pin img').alt = ad.offer.title;
 
   return adsElement;
 };
