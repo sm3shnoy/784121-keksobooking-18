2c4c2a3e550326b75ed15dfb3db450337b062f52
sm3shnoy <eskey96@inbox.ru>
2019-09-25 12:54:28 +0400

    Подставляет правильные моковые данные

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// Переменные для объектов в предложении (offer)
-var TITLE = 'Заголовок';
-var PRICE = 100000;
-var TYPE = 'palace';
-var ROOMS = 3;
-var GUESTS = 5;
-var CHECKIN = '12:00';
-var CHECKOUT = '13:00';
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var DESCRIPTION = 'Описание';
-var PHOTO = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
 // Координаты для позиционирования (location)
 var minY = 130;
 var maxY = 630;
@@ -35,26 +23,64 @@ var randomNumber = function (min, max) {
 
 // Генерируем объекты объявлений
 var adsGenerator = function () {
-  var ads = [];
+  var MOCK = [];
 
   for (var i = 0; i <= COUNTER_ADS; i++) {
-    ads[i] = {
+    MOCK[i] = {
       'author': {
-        'avatar': 'img/avatars/user' + '0' + i + '.png'
+        'avatar': [
+          'img/avatars/user01.png',
+          'img/avatars/user02.png',
+          'img/avatars/user03.png',
+          'img/avatars/user04.png',
+          'img/avatars/user05.png',
+          'img/avatars/user06.png',
+          'img/avatars/user07.png',
+          'img/avatars/user08.png'
+        ]
       },
 
       'offer': {
-        'title': TITLE,
+        'title': [
+          'Большая квартира в центре',
+          'Маленькая квартира на окраине',
+          'Царские аппартаменты',
+          'Нецарские аппартаменты',
+          'Старый маленький дом',
+          'Большой дом с бассейном',
+          'Шалаш на дереве',
+          'Уютное бунгало'
+        ],
         'address': '{{location.x}}, {{location.y}}',
-        'price': PRICE,
-        'type': TYPE[0],
-        'rooms': ROOMS,
-        'guests': GUESTS,
-        'checkin': CHECKIN,
-        'checkout': CHECKOUT,
-        'features': FEATURES[0],
-        'description': DESCRIPTION,
-        'photos': PHOTO
+        'price': {
+          min: 1000,
+          max: 100000
+        },
+        'type': ['palace', 'flat', 'house', 'bungalo'],
+        'rooms': {
+          min: 1,
+          max: 5
+        },
+        'guests': {
+          min: 1,
+          max: 10
+        },
+        'checkin': ['12:00', '13:00', '14:00'],
+        'checkout': ['12:00', '13:00', '14:00'],
+        'features': [
+          'wifi',
+          'dishwasher',
+          'parking',
+          'washer',
+          'elevator',
+          'conditioner'
+        ],
+        'description': '',
+        'photos': [
+          'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+          'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+          'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+        ]
       },
 
       'location': {
@@ -64,7 +90,7 @@ var adsGenerator = function () {
     };
   }
 
-  return ads;
+  return MOCK;
 };
 
 // Убираем класс faded у блока map
@@ -72,22 +98,22 @@ var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 // Все объявления
-var ads = adsGenerator();
+var MOCK = adsGenerator();
 
 var renderedAds = function (ad) {
   var adsElement = similarPinTemplate.cloneNode(true);
 
   adsElement.style.left = ad.location.x + 'px';
   adsElement.style.top = ad.location.y + 'px';
-  adsElement.querySelector('img').src = ad.author.avatar;
-  adsElement.querySelector('img').alt = ad.offer.title;
+  adsElement.querySelector('img').src = ad.author.avatar[randomNumber(0, ad.author.avatar.length - 1)];
+  adsElement.querySelector('img').alt = ad.offer.title[randomNumber(0, ad.offer.title.length - 1)];
 
   return adsElement;
 };
 
 // Создаем фрагмент для вставки на страницу
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < ads.length; i++) {
-  fragment.appendChild(renderedAds(ads[i]));
+for (var i = 0; i < MOCK.length; i++) {
+  fragment.appendChild(renderedAds(MOCK[i]));
 }
 similarListElement.appendChild(fragment);
